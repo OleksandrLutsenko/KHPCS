caeb1fd1dbc68bd088117c66a8c4d3452dcbd4e5
Polous-Sergey <polous@ro.ru>
2017-10-20 13:17:29 +0300

    Added navigation, fixed login page

---
+++ app/templates/tabs/tabs.controller.js
@@ -0,0 +1,15 @@
+;(function () {
+    'use strict';
+    angular.module('app')
+        .controller('TabsController', TabsController);
+
+    TabsController.$inject = [];
+
+    function TabsController() {
+        let vm = this
+
+
+
+    }
+
+})();
