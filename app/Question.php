f3ff5c3b60b9e80fda0c5c46928349426ccf3139
Ivanenko Oleg <ivanenko.oleg.m@gmail.com>
2017-10-25 19:10:04 +0300

    Report in process

--- app/Question.php
+++ app/Question.php
@@ -3,12 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Question extends Model
 {
-    protected $fillable = ['title'];
+    protected $fillable = ['title', 'type'];
+
+    protected $visible = ['title', 'answers'];
+
+    protected $appends = ['answers'];
 
     public function block(){
         return $this->belongsTo(Block::class);
+    }
+
+    public function answer(){
+        return $this->hasMany(Answer::class);
+    }
+
+    public function getAnswersAttribute()
+    {
+        return $this->answer;
     }
 }
