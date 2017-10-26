4bc659c26bf9cb616cd227c3fed7c65000a8dcac
SergeyK <skinsidex@gmail.com>
2017-10-26 14:15:17 +0300

    code beauty on report policy

--- app/Policies/ReportPolicy.php
+++ app/Policies/ReportPolicy.php
@@ -17,10 +17,12 @@ class ReportPolicy
     {
         return $user->isAdmin();
     }
+
     public function update(User $user)
     {
         return $user->isAdmin();
     }
+
     public function delete(User $user)
     {
         return $user->isAdmin();
