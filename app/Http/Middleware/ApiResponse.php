f760fa5d6e28e175a9825ec76d27ef79c00cbf58
Dmytro <dmitriytris@gmail.com>
2017-10-31 17:39:08 +0200

    merge with CodeReview branch

---
+++ app/Http/Middleware/ApiResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ApiResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+
+//        return response()->json([
+//            'success' => $response->status() == 200,
+//            'status' => $response->status(),
+//            'data' => $response->getOriginalContent(),
+//            'errors' => [],
+//        ]);
+    }
+}
