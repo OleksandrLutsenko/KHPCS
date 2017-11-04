ebd04f2a17654847bb3fc5fd933051be1c19a56b
Polous-Sergey <polous@ro.ru>
2017-11-04 14:17:36 +0200

    add $broadcast to survey block

--- app/templates/survey-block/survey-block.controller.js
+++ app/templates/survey-block/survey-block.controller.js
@@ -3,18 +3,22 @@
     angular.module('app')
         .controller('SurveyBlockController', SurveyBlockController);
 
-    SurveyBlockController.$inject = ['userService', '$state', 'survey'];
+    SurveyBlockController.$inject = ['userService', '$state', 'survey', '$scope'];
 
-    function SurveyBlockController(userService, $state, survey) {
+    function SurveyBlockController(userService, $state, survey, $scope) {
         let vm = this;
         vm.setActiveBlock = setActiveBlock;
-
         let id = survey.getActineSurvey();
 
         vm.items = userService.getItems()[id.indexSurvey].blocks;
 
+        survey.setActiveBlock(vm.items[0].id, 0);
+        console.log(vm.items[0].id);
+        $state.go('tab.survey-block.survey-question');
+
         function setActiveBlock(id, indexBlock) {
             survey.setActiveBlock(id, indexBlock);
+            $scope.$broadcast('parent', indexBlock);
         }
     }
 })();
--- app/templates/survey-question/survey-question.controller.js
+++ app/templates/survey-question/survey-question.controller.js
@@ -3,18 +3,77 @@
     angular.module('app')
         .controller('SurveyQuestionController', SurveyQuestionController);
 
-    SurveyQuestionController.$inject = ['userService', '$state', 'survey'];
+    SurveyQuestionController.$inject = ['userService', '$state', 'survey', '$scope', '$mdDialog'];
 
-    function SurveyQuestionController(userService, $state, survey) {
+    function SurveyQuestionController(userService, $state, survey, $scope, $mdDialog) {
         let vm = this;
         let idS = survey.getActineSurvey();
         let idB = survey.getActiveBlock();
+        let indexBlock = idB.indexBlock;
 
-        vm.items = userService.getItems()[idS.indexSurvey].blocks[idB.indexBlock].questions;
-        console.log(vm.items);
+        $scope.$on('parent', function(event, data){
+            indexBlock = data;
+            vm.items = userService.getItems()[idS.indexSurvey].blocks[indexBlock].questions;
+        });
 
-        function asdf(id, indexBlock) {
-            survey.setActiveBlock(id, indexBlock)
-        }
+        vm.items = userService.getItems()[idS.indexSurvey].blocks[indexBlock].questions;
+
+        // function asdf(id, indexBlock) {
+        //     survey.setActiveBlock(id, indexBlock)
+        // }
+
+        vm.showEdit = function (id, index) {
+
+            $mdDialog.show({
+                controller: EditController,
+                controllerAs: 'vm',
+                templateUrl: 'templates/survey-question/edit.html',
+                clickOutsideToClose: true
+            });
+
+            function EditController($mdDialog) {
+                let vs = this;
+
+                if(typeof id != 'undefined') {
+                    // vs.data =  {
+                    //     name: vm.customers[index].name,
+                    //     surname: vm.customers[index].surname,
+                    //     classification: vm.customers[index].classification
+                    // }
+                }
+
+                vs.saved = function () {
+                    console.log(id, vs.data);
+                    if(typeof id != 'undefined') {
+                        userService.updateCustomers(id, vs.data).then(function (res) {
+                            if (res.success) {
+                                console.log(res, 'succes');
+                                vm.customers.splice(index, 1, res.data);
+                            }
+                            else {
+                                console.log('error');
+                            }
+                            vs.cancel();
+                        });
+                    }
+                    else {
+                        userService.createCustomers(vs.data).then(function (res) {
+                            if (res.success) {
+                                console.log(res, 'succes');
+                                vm.customers.push(res.data);
+                            }
+                            else {
+                                console.log('error');
+                            }
+                            vs.cancel();
+                        });
+                    }
+                };
+
+                vs.cancel = function () {
+                    $mdDialog.cancel();
+                };
+            }
+        };
     }
 })();
