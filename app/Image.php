1f3e54e3d163fac31f398b8ab2c00fb11be3611a
Dmytro <dmitriytris@gmail.com>
2017-11-30 11:36:26 +0200

    contract fix

--- app/Image.php
+++ app/Image.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    protected $fillable = ['link'];
+    protected $fillable = ['link', 'contract_id'];
 
     public function contract(){
         return $this->belongsTo(Contract::class);
