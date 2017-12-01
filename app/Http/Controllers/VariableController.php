c7da5938f655edda81a414440777af1abc7cb79b
Dmytro <dmitriytris@gmail.com>
2017-12-01 13:29:24 +0200

    variables, contract view and download update

---
+++ app/Http/Controllers/VariableController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Variable;
+use Illuminate\Http\Request;
+
+class VariableController extends Controller
+{
+    /**
+     * @param Variable $variable
+     * @param User $user
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Database\Eloquent\Collection|\Symfony\Component\HttpFoundation\Response|static[]
+     * @internal param Survey $survey
+     */
+    public function index(Variable $variable, User $user)
+    {
+        return Variable::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request|Request $request
+     * @param Variable $variable
+     * @param User $user
+     * @return \Illuminate\Http\Response
+     * @internal param Survey $survey
+     */
+    public function store(Request $request, Variable $variable, User $user)
+    {
+        $variable = $variable->create($request->all());
+        return compact('variable');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Variable $variable
+     * @param User $user
+     * @return \Illuminate\Http\Response
+     * @internal param Survey $survey
+     * @internal param int $id
+     */
+    public function show(Variable $variable, User $user)
+    {
+        return compact('variable');
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Variable $variable
+     * @param User $user
+     * @return \Illuminate\Http\Response
+     * @internal param Survey $survey
+     * @internal param int $id
+     */
+    public function update(Request $request, Variable $variable, User $user)
+    {
+        $variable->update($request->all());
+        return compact('variable');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Variable $variable
+     * @param User $user
+     * @return \Illuminate\Http\Response
+     * @internal param Survey $survey
+     * @internal param int $id
+     */
+    public function destroy(Variable $variable, User $user)
+    {
+        $variable->delete();
+        return compact('variable');
+    }
+}
---
+++ app/Variable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
+
+class Variable extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['text', 'user_id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($table) {
+            $table->user_id = Auth::user()->id;
+        });
+    }
+}
